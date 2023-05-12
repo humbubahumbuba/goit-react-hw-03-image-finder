d426dde33f8de98f94f01560a052a5771e048732
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-05-12 21:25:21 +0300

    thisState_loadedImages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,7 +25,7 @@ export class App extends Component {
   };
 
   componentDidUpdate = (_, prevState) => {
-    const { page, loadedImages, searchQuery } = this.state;
+    const { page, searchQuery } = this.state;
     const prevQueryValue = prevState.searchQuery;
     const currentQueryValue = searchQuery;
 
@@ -37,7 +37,7 @@ export class App extends Component {
           this.showNotification(data);
 
           this.setState({
-            loadedImages: [...loadedImages, ...data.hits],
+            loadedImages: [...this.state.loadedImages, ...data.hits],
             status: 'resolved',
             totalPages: Math.floor(data.totalHits / 12),
           });
